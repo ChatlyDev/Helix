e847acf70f180d21b62bff84df4ea1dc1eee426a
Chris Logue <chrisloguedev@gmail.com>
2023-03-02 22:08:08 -0500

    begin adding kmers with differences

--- bioinformatics.py
+++ bioinformatics.py
@@ -14,7 +14,7 @@ tgataatgaatttacatgcttccgcgacgatttacctcttgatcatcgatccgattgaag
 atcttcaattgttaattctcttgcctcgactcatagccatgatgagctcttgatcatgtt
 tccttaaccctctattttttacggaagaatgatcaagctgctgctcttgatcatcgtttc"""
 
-human_dna = pd.read_table("input/dna/human.txt")
+#human_dna = pd.read_table("input/dna/human.txt")
 
 base_pairs = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'T'}
 
@@ -29,8 +29,10 @@ def find_kmers(dna, filter_size: int):
         if(seq_list.count(segment) > 0):
             if(kmer_list.get(segment)):
                 kmer_list[segment] = kmer_list[segment] + 1
+            #once it appears a second time we add it to the kmer list with a count of 2
             else:
                 kmer_list[segment] = 2
+        #since we havent sequenced this yet we will add it to the list
         else:
             seq_list.append(segment)
             
@@ -40,6 +42,33 @@ def find_kmers(dna, filter_size: int):
             del kmer_list[key]
             
     return kmer_list
+
+#find kmers with a variability of max_diff SNPs
+def find_kmers_with_differences(dna, filter_size: int, max_diff: int):
+    dna_len = len(dna)
+    print(f'sequencing dna of length {dna_len}')
+    seq_list = []
+    kmer_list = dict()
+    for i in range(0, (dna_len-filter_size)):
+        segment = dna[i:i+filter_size]
+        if(seq_list.count(segment) > 0):
+            if(kmer_list.get(segment)):
+                kmer_list[segment] = kmer_list[segment] + 1
+            #once it appears a second time we add it to the kmer list with a count of 2
+            else:
+                kmer_list[segment] = 2
+        #since we havent sequenced this yet we will add it to the list
+        else:
+            seq_list.append(segment)
+            
+    #kind of shit until i feel like making a temporary dictionary to put sequences that only appear once
+    for key in kmer_list:
+        if(kmer_list[key] < 2):
+            del kmer_list[key]
+            
+    return kmer_list
+
+    
 
 def compliment(kmer):
     compliment = ""
